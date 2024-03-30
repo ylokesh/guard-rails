b2c30ed1bef82c98c6c8f4f7d581c8451cef8287
ylokesh <yadav.lokesh@gmail.com>
2024-03-30 21:19:54 +0530

    1

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,13 @@ module.exports = {
     upload: {
       target: "temporary-public-storage",
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.8 }],
+        "categories:best-practices": ["error", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 0.8 }],
+      },
+    },
   },
 };
