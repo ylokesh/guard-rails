a83fac69c8368e839b34cd02dcbe7d9941a317a7
ylokesh <yadav.lokesh@gmail.com>
2024-03-30 22:06:23 +0530

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://placehold.co/6000x4000" alt="presentation" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
